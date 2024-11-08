718197ca56c20eded11633c9f7d089873ef3a09a
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-08 14:15:44 -0300

    08/11/2024 14:15

--- scripts/script_inicial.js
+++ scripts/script_inicial.js
@@ -16,23 +16,40 @@ function incializaçãoSite() {
     var headerSection = document.querySelector('section#secHeader')
     var h1Header = document.createElement('h1')
     headerSection.appendChild(h1Header)
-
     var menuHeader = document.createElement('nav')
-
     headerSection.appendChild(menuHeader)
 
     //primeira frase
-    h1Header.innerText = 'Olá, Mundo!'
+    function passoUm() {
+        h1Header.innerText = 'Olá, Mundo!'
+        h1Header.style.animation = "typing 2s steps(11), cursor .6s step-end infinite alternate";
+    }
 
     //segunda frase
-    h1Header.innerText = 'Eu sou LaLunaInSky'
+    function passoDois() {
+        h1Header.innerText = 'Eu sou LaLunaInSky'
+        h1Header.style.animation = "typing 2s steps(18), cursor .6s step-end infinite alternate";
+    }
 
     //terceira frase
-    h1Header.innerText = 'Bem Vindo(a)!'
+    function passoTres() {
+        h1Header.innerText = 'Bem Vindo(a)!'
+        h1Header.style.animation = "typing 2s steps(13), cursor .6s step-end infinite alternate";
+    }
 
     //titulo + menu
-    h1Header.innerText = 'LaLunaInSky'
-    MenusOpções(menuHeader)
+    function passoQuatro() {
+        h1Header.innerText = 'LaLunaInSky'
+        h1Header.style.animation = "typing 2s steps(11), cursor .6s step-end infinite alternate";
+
+        function proximoPasso() {
+            MenusOpções(menuHeader)
+        }
+    }
+
+    var listaPassos = [passoUm, passoDois, passoTres, passoQuatro]
+    var contagem = 0
+    h1Header.innerText = `${contagem}`
 }
 
 incializaçãoSite()
