71bd9f54ea798c1504d2bc8c96285fd3407c3750
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-08 17:03:27 -0300

    08/11/2024 17:03

--- scripts/script_repositórios.js
+++ scripts/script_repositórios.js
@@ -16,24 +16,15 @@ function MenusOpções(menu) {
     }
 }
 
-function incializaçãoSite() {
+function incializaçãoHeader() {
     var contagem = 0
     var headerSection = document.querySelector('section#secHeader')
     var h1Header = document.createElement('h1')
-    h1Header.innerText ='LaLunaInSky'
+    h1Header.innerHTML =`<a href="../index.html" target="_self">LaLunaInSky</a>`
     headerSection.appendChild(h1Header)
     var menuHeader = document.createElement('nav')
     headerSection.appendChild(menuHeader)
-    menuHeader.style.animation = `typing 2s steps(${65}), cursor .6s step-end infinite alternate`;
     MenusOpções(menuHeader)
-
-    function tirarBorda() {
-        h1Header.style.borderRight = '0px';
-        menuHeader.style.borderRight = '0px';
-        clearInterval(intervalo)
-    }
-    
-    var intervalo = setInterval(tirarBorda, 5000)
 }
 
-incializaçãoSite()
+incializaçãoHeader()
