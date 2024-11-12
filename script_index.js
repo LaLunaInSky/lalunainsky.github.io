f56cbe2dfaa1e0a5353ad27ccaa1f27bbc22a36a
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-12 07:12:15 -0300

    12/11/2024 07:12

--- script_index.js
+++ script_index.js
@@ -1,44 +1,28 @@
-function MenusOpções(menu) {
-    var listaDeOpçõesMenu = ['Sobre', 'Repositórios', 'Projetos', 'Contato',]
-
-    var quantidadeDeLinksDoMenu = listaDeOpçõesMenu.length
-
-    for (var count = 0; count < quantidadeDeLinksDoMenu; count++) {
-        var link = document.createElement('a')
-        if (count == 0) {
-            link.setAttribute('id', 'primeiraOcorrênciaNav')
-            link.setAttribute('href', 'pages/page_sobre/sobre.html')
-        } else if (count == 1) {
-            link.setAttribute('href', 'pages/page_repositórios/repositórios.html')
-        } else if (count == 2) {
-            link.setAttribute('href', 'pages/page_projetos/projetos.html')
-        } else if (count == 3) {
-            link.setAttribute('href', 'pages/page_contato/contato.html')
-        }
-        link.setAttribute('target', '_self')
-        link.innerText = `${listaDeOpçõesMenu[count]}`
-        menu.appendChild(link)
-    }
-}
-
-function incializaçãoSite() {
-    var contagem = 0
-    var headerSection = document.querySelector('section#secHeader')
-    var h1Header = document.createElement('h1')
-    h1Header.innerText ='LaLunaInSky'
-    headerSection.appendChild(h1Header)
-    var menuHeader = document.createElement('nav')
-    headerSection.appendChild(menuHeader)
-    menuHeader.style.animation = `typing 2s steps(${40}), cursor .6s step-end infinite alternate`;
-    MenusOpções(menuHeader)
-
-    function tirarBorda() {
-        h1Header.style.borderRight = '0px';
-        menuHeader.style.borderRight = '0px';
-        clearInterval(intervalo)
-    }
+function aparecimentoSections() {
+    var sec = document.getElementById('primeiro')
+    sec.style.visibility = 'visible';
     
-    var intervalo = setInterval(tirarBorda, 5000)
+    function passoDois() {
+        sec.style.visibility = 'hidden';
+        sec = document.getElementById('segundo')
+        sec.style.visibility = 'visible';
+    }
+
+    function passoTres() {
+        sec.style.visibility = 'hidden';
+        sec = document.getElementById('terceiro')
+        sec.style.visibility = 'visible';
+    }
+
+    function passoQuatro() {
+        sec.style.visibility = 'hidden';
+        sec = document.getElementById('quarto')
+        //var nav = document.querySelector('nav')
+        sec.style.visibility = 'visible';
+        //nav.style.visibility = 'hidden';
+    }
+
+    passoQuatro()
 }
 
-incializaçãoSite()
+aparecimentoSections()
