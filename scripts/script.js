2b6773ba4148f863716b2824354cd511d12e5cfb
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-22 20:19:19 -0300

    22/11/2024 20:19

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,21 @@
 function aparecerSumirOpções(num) {
     let articleEstudos = document.querySelector('article.estudos')
-    let
+    let articleProjetos = document.querySelector('article.projetos')
     
-    if (num == 0) {
-        if (articleEstudos.style.display != 'none') {
-            articleEstudos.style.display = 'none'
-        } else {
-            articleEstudos.style.display = 'block'
-        }
+    switch (num) {
+        case 0:    
+            if (articleEstudos.style.display != 'none') {
+                articleEstudos.style.display = 'none'
+            } else {
+                articleEstudos.style.display = 'block'
+            }
+            break
+        case 1:
+            if (articleProjetos.style.display != 'none') {
+                articleProjetos.style.display = 'none'
+            } else {
+                articleProjetos.style.display = 'block'
+            }
+            break
     }
 }
