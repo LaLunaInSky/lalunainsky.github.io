100377a2a2777b2366108a1dc76082d95c03f63a
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-24 11:01:17 -0300

    23/11/2024 11:01

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,15 @@
-function alteraçãoDarkLightMode(num=1) {
+function alteraçãoDarkLightMode() {
     let root = document.querySelector(':root')
+    let imgHeaderDarkLightMode = document.querySelector('img#darkLightModeImgem')
+    let rootStyle = getComputedStyle(root)
 
-    switch (num) {
-        case 0:
-            //black
-            root.style.setProperty('--pretoBranco', '0')
-            root.style.setProperty('--darkLight', 'black')
-            break
-        case 1:
-            //white
-            root.style.setProperty('--pretoBranco', '1')
-            root.style.setProperty('--darkLight', 'white')
-            break
+    if (rootStyle.getPropertyValue('--pretoBranco') == 1 /*white*/) {
+        root.style.setProperty('--pretoBranco', '0')
+        root.style.setProperty('--darkLight', 'black')
+        imgHeaderDarkLightMode.src = 'imagens/icones_dark_light_mode/moon_32px.png'
+    } else {
+        root.style.setProperty('--pretoBranco', '1')
+        root.style.setProperty('--darkLight', 'white')
+        imgHeaderDarkLightMode.src = 'imagens/icones_dark_light_mode/sun_32px.png'
     }
 }
-
-alteraçãoDarkLightMode()
