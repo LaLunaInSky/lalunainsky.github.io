35aad236a9146dfacc655652d698b6bf334afec7
LaLunaInSky <moonaflordapele@gmail.com>
2024-12-20 11:02:23 -0300

    20/12/2024 11:02

--- scripts/pages/projetos/script_main.js
+++ scripts/pages/projetos/script_main.js
@@ -1,1 +1,18 @@
 "use strict"
+
+let mostradorProjetoÚnico = document.querySelector('.mostradorProjeto')
+let mostradorProjetos = document.querySelector('.projetos')
+
+function abrirProjeto(nomeProjeto) {
+    let nomeDoProjeto = nomeProjeto
+    let mostradorProjetoIframe = document.querySelector('.mostradorProjeto>iframe')
+
+    mostradorProjetoIframe.setAttribute('src', `https://lalunainsky.github.io/${nomeDoProjeto}/`)
+    mostradorProjetoÚnico.style.display = 'flex'
+    mostradorProjetos.style.display = 'none'
+}
+
+function fecharMostradorProjeto() {
+    mostradorProjetoÚnico.style.display = 'none'
+    mostradorProjetos.style.display = 'block'
+}
