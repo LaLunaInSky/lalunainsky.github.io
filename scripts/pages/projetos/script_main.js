4fab42a2acd812427ff82287943ef25e7fb017a8
LaLunaInSky <moonaflordapele@gmail.com>
2024-12-20 11:08:27 -0300

    20/12/2024 11:08

--- scripts/pages/projetos/script_main.js
+++ scripts/pages/projetos/script_main.js
@@ -1,6 +1,8 @@
 "use strict"
 
 let mostradorProjetoÚnico = document.querySelector('.mostradorProjeto')
+let header = document.querySelector('header')
+let footer = document.querySelector('footer')
 let mostradorProjetos = document.querySelector('.projetos')
 
 function abrirProjeto(nomeProjeto) {
@@ -10,9 +12,13 @@ function abrirProjeto(nomeProjeto) {
     mostradorProjetoIframe.setAttribute('src', `https://lalunainsky.github.io/${nomeDoProjeto}/`)
     mostradorProjetoÚnico.style.display = 'flex'
     mostradorProjetos.style.display = 'none'
+    header.style.display = 'none'
+    footer.style.display = 'none'
 }
 
 function fecharMostradorProjeto() {
     mostradorProjetoÚnico.style.display = 'none'
     mostradorProjetos.style.display = 'block'
+    header.style.display = 'flex'
+    footer.style.display = 'block'
 }
