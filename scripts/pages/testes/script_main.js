c9319933d53a2df5e1b7e74547286da35291bf10
LaLunaInSky <moonaflordapele@gmail.com>
2024-12-21 10:32:08 -0300

    21/12/2024 10:32

---
+++ scripts/pages/testes/script_main.js
@@ -0,0 +1,24 @@
+"use strict"
+
+let menuLateralAbertura = false
+let opçõesMenuLateral = document.querySelector('.menuLateral>nav')
+let imgMenuLateral = document.querySelector('.menuLateral>section>img')
+
+function trueOrFalseMenuLateralAbertura() {
+    if (menuLateralAbertura == false) {
+        menuLateralAbertura = true
+    } else {
+        menuLateralAbertura = false
+    }
+
+    switch (menuLateralAbertura) {
+        case true:
+            opçõesMenuLateral.style.display = 'flex'
+            imgMenuLateral.setAttribute('src', '../../imagens/menu_icones/close_icon_50px.png')
+            break
+        case false:
+            opçõesMenuLateral.style.display = 'none'
+            imgMenuLateral.setAttribute('src', '../../imagens/menu_icones/menu_icon_50px.png')
+            break
+    }
+}
