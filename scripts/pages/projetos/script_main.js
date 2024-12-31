d6b2efedca1b0858be1232a826e759c5bdcfc9bc
LaLunaInSky <moonaflordapele@gmail.com>
2024-12-31 03:01:21 -0300

    31/12/2024 03:01

--- scripts/pages/projetos/script_main.js
+++ scripts/pages/projetos/script_main.js
@@ -7,9 +7,33 @@ let redesSociaisMenuLateral = document.querySelectorAll('.redesSociaisMenuLatera
 let imgMenuLateral = document.querySelector('.menuLateral>section>img')
 let date = new Date()
 let anoAtual = date.getFullYear()
+const projetoMostrador = document.querySelector('.projetoMostrar')
+const mainProjetos = document.querySelector('main')
 let footer = document.querySelector('footer>p')
-
 footer.innerText = `${anoAtual} - LaLunaInSky`
+let headerPágina = document.querySelector('header')
+let fotterPágina = document.querySelector('footer')
+
+let mostradorProjeto = (nomeProjeto) => {
+    let iframe =  document.querySelector('iframe')
+    iframe.setAttribute('src', `https://www.lalunainsky.com/projeto-${nomeProjeto}/`)
+
+    let linkRepoProjeto = document.querySelector('.fechadorProjetoMostrador > p > a')
+    linkRepoProjeto.setAttribute('href', `https://github.com/LaLunaInSky/projeto-${nomeProjeto}/`)
+
+    projetoMostrador.style.display = 'flex'
+    mainProjetos.style.display = 'none'
+    headerPágina.style.display = 'none'
+    fotterPágina.style.display = 'none'
+    /*falta esconder o header, foooter*/
+}
+
+let fecharMostradorProjeto = () => {
+    projetoMostrador.style.display = 'none'
+    mainProjetos.style.display = 'block'
+    headerPágina.style.display = 'flex'
+    fotterPágina.style.display = 'block'
+}
 
 function trueOrFalseMenuLateralAbertura() {
     if (menuLateralAbertura == false) {
