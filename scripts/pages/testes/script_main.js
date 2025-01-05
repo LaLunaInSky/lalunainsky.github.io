15b78e048aaca61355c82fc443d8f7da669d7d41
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-05 08:59:34 -0300

    05/01/2025 08:59

--- scripts/pages/testes/script_main.js
+++ scripts/pages/testes/script_main.js
@@ -1,1 +1,14 @@
-"use strict"
+const mudarTamanhoFotoSobre = () => {
+    let imgFotoSobre = document.querySelector('.sobre img')
+    if (window.innerWidth >= 992) {
+        imgFotoSobre.setAttribute('src', '../../imagens/foto_perfil/foto_lalunainsky_300px.jpg')
+    } else {
+        imgFotoSobre.setAttribute('src', '../../imagens/foto_perfil/foto_lalunainsky_200px.jpg')
+    }
+}
+
+window.onresize = function(event) {
+    mudarTamanhoFotoSobre()
+}
+
+mudarTamanhoFotoSobre()
