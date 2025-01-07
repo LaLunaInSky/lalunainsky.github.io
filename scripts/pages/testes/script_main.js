7c22390997cbbd7519f555cb8664604a328658d0
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-07 15:52:18 -0300

    07/01/2025 15:52

---
+++ scripts/pages/testes/script_main.js
@@ -0,0 +1,24 @@
+"use strict"
+
+let organizaçãoMods = ()=>{
+    let h2IDm1 = document.querySelector('#m1>h2')
+    if (window.innerWidth >= 850) {
+        let quantidadeDeMódulos = document.querySelector('.módulos')
+        if (quantidadeDeMódulos.children.length < 2) {
+            quantidadeDeMódulos.style.justifyContent = 'center'
+            h2IDm1.style = 'margin-top: 0px;border-radius: 0px 0px 20px 20px;'
+        } else {
+            quantidadeDeMódulos.style.justifyContent = 'space-around'
+            h2IDm1.style = 'margin-top: 20px;border-radius: 20px;'
+        }
+    } else {
+        h2IDm1.style = 'margin-top: 0px;border-radius: 0px 0px 20px 20px;'
+    }
+}
+
+window.onresize = function(evento) {
+    organizaçãoMods()
+    console.log(window.innerWidth)
+}
+
+organizaçãoMods()
